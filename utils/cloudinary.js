feb78053941ae175c2e2793c2a1c2a9e5068546d
moin143264 <119032664+moin143264@users.noreply.github.com>
2025-05-26 19:19:53 +0530

    Delete utils/cloudinary.js

--- utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-    cloud_name: 'utran-app',
-    api_key: '414738459261494',
-    api_secret: 'bzZZeqmXUIBWWXaf0P8mwEruc1s'
-});
-
-module.exports = cloudinary;
