3cb0d5cf5e9bbf91554ebaab589ef24a43ccf474
moin143264 <119032664+moin143264@users.noreply.github.com>
2025-05-27 10:07:14 +0530

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -11,19 +11,35 @@ cloudinary.config({
 
 // Generate signature for client-side upload
 const generateSignature = (timestamp) => {
-    const params = {
+    // The parameters that will be signed
+    const paramsToSign = {
         timestamp: timestamp,
         upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET || 'ml_default',
         folder: 'user-profiles',
-        allowed_formats: ['jpg', 'jpeg', 'png'],
+        allowed_formats: ['jpg', 'jpeg', 'png'].join(','),
         max_file_size: 10485760 // 10MB
     };
     
     // Remove any undefined values
-    Object.keys(params).forEach(key => params[key] === undefined && delete params[key]);
+    Object.keys(paramsToSign).forEach(key => {
+        if (paramsToSign[key] === undefined || paramsToSign[key] === '') {
+            delete paramsToSign[key];
+        }
+    });
     
-    const signature = cloudinary.utils.api_sign_request(params, process.env.CLOUDINARY_API_SECRET);
-    return { signature, params };
+    // Generate the signature
+    const signature = cloudinary.utils.api_sign_request(
+        paramsToSign, 
+        process.env.CLOUDINARY_API_SECRET
+    );
+    
+    return { 
+        signature, 
+        timestamp,
+        ...paramsToSign,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'utran-app'
+    };
 };
 
 module.exports = {
