8add653a4f0b56727737bdb56dd2b79dbf459f4f
moin143264 <119032664+moin143264@users.noreply.github.com>
2025-05-27 09:52:05 +0530

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -13,10 +13,17 @@ cloudinary.config({
 const generateSignature = (timestamp) => {
     const params = {
         timestamp: timestamp,
-        upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET || 'ml_default'
+        upload_preset: process.env.CLOUDINARY_UPLOAD_PRESET || 'ml_default',
+        folder: 'user-profiles',
+        allowed_formats: ['jpg', 'jpeg', 'png'],
+        max_file_size: 10485760 // 10MB
     };
+    
+    // Remove any undefined values
+    Object.keys(params).forEach(key => params[key] === undefined && delete params[key]);
+    
     const signature = cloudinary.utils.api_sign_request(params, process.env.CLOUDINARY_API_SECRET);
-    return signature;
+    return { signature, params };
 };
 
 module.exports = {
